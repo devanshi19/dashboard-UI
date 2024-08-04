5249f9ef5e2a279d73ae067bb168e0e89aca7784
devanshi jadav <krutiksinh022@gmail.com>
2024-08-04 21:53:43 +0530

    Sidebar ui change

--- src/components/Header/Sidebar/Sidebar.js
+++ src/components/Header/Sidebar/Sidebar.js
@@ -6,25 +6,48 @@ import { BiExport } from "react-icons/bi";
 import "./Sidebar.scss"
 const Sidebar = () => {
     return (
-        <div className="sidebar-wrap">
-            <ul>
-                <li className="active">
-                    <AiFillHome className="icon" />
-                </li>
-                <li>
-                    <IoStatsChart className="icon" />
-                </li>
-                <li>
-                    <BiTask className="icon" />
-                </li>
-                <li>
-                    <IoBagCheckOutline className="icon" />
-                </li>
-            </ul>
-            <div className="export-icon">
-                <BiExport className="icon" />
+        <>
+            <div className="sidebar-wrap">
+                <ul>
+                    <li className="active">
+                        <AiFillHome className="icon" />
+                    </li>
+                    <li>
+                        <IoStatsChart className="icon" />
+                    </li>
+                    <li>
+                        <BiTask className="icon" />
+                    </li>
+                    <li>
+                        <IoBagCheckOutline className="icon" />
+                    </li>
+                </ul>
+                <div className="export-icon">
+                    <BiExport className="icon" />
+                </div>
+
             </div>
-        </div>
+            <div className="mobile-sidebar">
+                <ul>
+                    <li className="active">
+                        <AiFillHome className="icon" />
+                    </li>
+                    <li>
+                        <IoStatsChart className="icon" />
+                    </li>
+                    <li>
+                        <BiTask className="icon" />
+                    </li>
+                    <li>
+                        <IoBagCheckOutline className="icon" />
+                    </li>
+                    <li>
+                        <BiExport className="icon" />
+                    </li>
+                </ul>
+            </div>
+        </>
+
     )
 }
 export default Sidebar;
